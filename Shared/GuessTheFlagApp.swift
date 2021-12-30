7aee359701ed26dca9793f8a6c7fa9fc16b3f3fe
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-30 16:51:23 +0900

    Initial Commit

---
+++ Shared/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2021/12/30.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
